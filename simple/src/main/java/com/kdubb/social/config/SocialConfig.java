9a38a078aaae13f03bcf30b94425fb60baec1ea1
Erich Reich <erich.reich@gmail.com>
2014-04-06 20:11:17 -0400

    minor fix

--- simple/src/main/java/com/kdubb/social/config/SocialConfig.java
+++ simple/src/main/java/com/kdubb/social/config/SocialConfig.java
@@ -59,7 +59,7 @@ public class SocialConfig {
 	@Bean
 	public ConnectionFactoryLocator connectionFactoryLocator() {
 		ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
-		registry.addConnectionFactory(new FacebookConnectionFactory(0
+		registry.addConnectionFactory(new FacebookConnectionFactory(
 				environment.getProperty("facebook.clientId"),
 				environment.getProperty("facebook.clientSecret")));
 		return registry;
